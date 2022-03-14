cbdc586c3dc3ffb36aac2b96b5d1774dca71e42a
Lael Andrade <laelandrade@gmail.com>
2022-03-14 16:37:19 -0300

    Usando padrão DTO para retornar usuários

--- src/main/java/com/educandoweb/workshopmongo/resources/UserResource.java
+++ src/main/java/com/educandoweb/workshopmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.educandoweb.workshopmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,26 +9,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.educandoweb.workshopmongo.DTO.UserDTO;
 import com.educandoweb.workshopmongo.domain.User;
 import com.educandoweb.workshopmongo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
-	
+
 	@Autowired
-	private UserService service;            //                    Incluindo o Service no Resource
+	private UserService service; // Incluindo o Service no Resource
 
 	@RequestMapping(method = RequestMethod.GET)
-	// ou pode usar também 
-	// @GetMapping
-	public ResponseEntity<List<User>> findAll() {
-//		List<User> list = new ArrayList<>();          Mudanças ->  Incluindo o Service no Resource
+	public ResponseEntity<List<UserDTO>> findAll() {
+		List<User> list = service.findAll();
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+	}
+	
+	
+//
+//  @GetMapping                                            // Sem incluir o Service no Resource
+//	public List<User> findAll() {
+//		List<User> list = new ArrayList<>();                
 //		User maria = new User("1001", "Maria Brown", "maria@gmail.com");
 //		User alex = new User("1002", "Alex Green", "alex@gmail.com");
+//   	List<User> list = new ArrayList<>();
 //		list.addAll(Arrays.asList(maria, alex));
-		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
-	}
-
+//      return list;
 }
